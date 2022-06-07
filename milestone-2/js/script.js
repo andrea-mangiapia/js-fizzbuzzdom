b687c73f0e264ef0a5b614bf932beee468fde503
andrea-mangiapia <andrea.mangiapia@gmail.com>
2022-06-07 19:13:44 +0200

    complete milestone 2

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -1,4 +1,4 @@
-//MILESTONE 1
+//MILESTONE 2
 
 const boxContainer = document.querySelector('.box-container')
 // Scrivo un programma che stampi i numeri da 1 a 100.
@@ -6,9 +6,10 @@ for(let i = 1; i <= 100; i++) {
 
     //Variabile per indicare sia multipli di 3 che di 5
     let FizzBuzz;
-    
-    // Per i multipli di 3 stampo Fizz al posto del numero e per i multipli di 5 stampo Buzz.
+
+    // Per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
     //Per i numeri che sono sia multipli di 3 che di 5 stampo FizzBuzz.
+
     if (i % 3 === 0 && i % 5 === 0) {
         FizzBuzz = 'FizzBuzz';
     } else if (i % 3 === 0) {
@@ -19,6 +20,11 @@ for(let i = 1; i <= 100; i++) {
         FizzBuzz = i
     }
 
-    // Stampato in console
-    console.log(FizzBuzz);
+    // Richiamo un elemento nel documento che corrisponde al selettore css specificato:
+    const boxList = document.querySelector('.am-list');
+
+    // Aggiungo gli li all'interno di boxList
+    const newList = `<li>${FizzBuzz}</li>`;
+    boxList.innerHTML += newList;
+
 }
